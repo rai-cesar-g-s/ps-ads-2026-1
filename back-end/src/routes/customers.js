70483479e5ac1ac353785be15f6b7ac8c40c3756
Raí César Gomes Da Silva <raigomes2022@gmail.com>
2026-04-01 20:41:06 +0000

    ENTREGA DA ATIVIDADE 1

--- back-end/src/routes/customers.js
+++ back-end/src/routes/customers.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import controller from '../controllers/customers.js'
+import controller from '../controllers/controller.js'
 const router = Router()
 
 
 router.post('/', controller.create)
 router.get('/', controller.retrieveAll)
+router.get('/:id', controller.retrieveOne)
+router.put('/:id', controller.update)
+router.delete('/:id', controller.delete)
 
 export default router
