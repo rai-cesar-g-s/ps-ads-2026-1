083924b4aae03c92aaa30aa7b82eee951e47bb5f
Raí César Gomes Da Silva <raigomes2022@gmail.com>
2026-06-11 14:53:51 +0000

    atualização do footer

--- front-end/src/ui/FooterBar.jsx
+++ front-end/src/ui/FooterBar.jsx
@@ -25,7 +25,7 @@ export default function FooterBar() {
           }
         }}
       >
-        Desenvolvido e mantido com <CoffeeIcon fontSize="small" /> por <a href="mailto:professor@faustocintra.com.br">Prof. Fausto Cintra</a>
+        Desenvolvido e mantido com <CoffeeIcon fontSize="small" /> por <a href="mailto:raigomes2022@gmail.com">Raí Cèsar</a>
       </Typography>
     </Box>
  </>
